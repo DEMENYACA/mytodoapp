c9955750e51b9b3dcd8ad1eec07dc909db927d92
Demenyaca <demenyaca@gmail.com>
2019-02-21 10:43:05 -0500

    Task added

--- add.php
+++ add.php
@@ -1,4 +1,24 @@
 <?php
-echo $_POST["task"];
+$task = $_POST["task"];
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "todoapp";
 
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql = "INSERT INTO mylist (task)
+    VALUES ('$task')";
+    // use exec() because no results are returned
+    $conn->exec($sql);
+    echo "New record created successfully";
+    }
+catch(PDOException $e)
+    {
+    echo $sql . "<br>" . $e->getMessage();
+    }
+
+$conn = null;
 ?>
