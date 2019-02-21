1fa47cb3a4f1bd435f0b906652810d48c94be570
Demenyaca <demenyaca@gmail.com>
2019-02-21 10:59:24 -0500

    Added Value

--- todoapp.php
+++ todoapp.php
@@ -2,3 +2,34 @@
     <input type="text" name="task">
     <input type="submit">
 </form>
+<?php
+    
+    
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "todoapp";
+    
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $stmt = $conn->prepare("SELECT * FROM mylist"); 
+        $stmt->execute();
+    
+        // set the resulting array to associative
+        $stmt->setFetchMode(PDO::FETCH_ASSOC);
+        $result = $stmt->fetchAll();
+        foreach ($result as $key => $value) {
+            echo $value["task"];
+        } 
+       
+    }
+    catch(PDOException $e) {
+        echo "Error: " . $e->getMessage();
+    }
+    $conn = null;
+    echo "</table>";
+    
+
+
+?>
